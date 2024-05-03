8bf9fd5bc0cb2b0a3e8e5807855d453012f635da
Grass2000 <164293953+Grass2000@users.noreply.github.com>
2024-05-03 13:07:05 +0700

    Update twrp_peridot.mk

--- twrp_peridot.mk
+++ twrp_peridot.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit from our custom product configuration.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from peridot device
